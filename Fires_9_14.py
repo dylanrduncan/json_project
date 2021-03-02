8d051753f3d03c268235f9f6e6cc3dd8662ed486
Dylan Duncan <Dylan_Duncan1@baylor.edu>
2021-03-02 17:53:01 -0600

    added US Fires programs to display maps of US fires with brightness over 450

---
+++ Fires_9_14.py
@@ -0,0 +1,41 @@
+import json
+
+infile = open("US_fires_9_14.json", "r")
+
+fire_data = json.load(infile)
+
+lons, lats, brights = [], [], []
+
+for fire in fire_data:
+    if fire["brightness"] > 450:
+        lon = fire["longitude"]
+        lat = fire["latitude"]
+        bright = fire["brightness"]
+        lons.append(lon)
+        lats.append(lat)
+        brights.append(bright)
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "text": brights,
+        "marker": {
+            "size": [0.03 * bright for bright in brights],
+            "color": brights,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Brightness"},
+        },
+    }
+]
+
+my_layout = Layout(title="US Fires 9-14-20 to 9-20-20")
+
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig, filename="US_Fires.html")
